5eba5827a1e506392e194f2c846ddadde4d13514
beck <linyuanhao1231@gmail.com>
2023-05-27 10:56:51 +0800

    實戰演練：套用多頁面路由與元件 routerlink練習

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
+import { AppComponent } from './app.component';
 import { Routes } from '@angular/router';
 import { ChartsComponent } from './charts/charts.component';
 import { TablesComponent } from './tables/tables.component';
+import { Component } from '@angular/core';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch:'full'
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  },
   {
     path:'charts',
     component: ChartsComponent
@@ -10,5 +23,15 @@ export const routes: Routes = [
   {
     path:'tables',
     component: TablesComponent
+  },
+  {
+    path:'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path:'**',
+    redirectTo: 'not-found',
+    pathMatch: 'full'
   }
+
 ];
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,13 +4,13 @@ import { chartPieDemo } from '../chart-pie-demo';
 import { chartAreaDemo } from '../chart-area-demo';
 
 @Component({
-  selector: 'app-charts',
+  selector: 'app-dashboard',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './charts.component.html',
-  styleUrls: ['./charts.component.css']
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
-export class ChartsComponent {
+export class DashboardComponent {
   ngOnInit(): void {
     chartPieDemo();
     chartAreaDemo();
