344c2d2176630549967b2e278463c8ba26c6ece7
beck <linyuanhao1231@gmail.com>
2023-05-27 15:21:34 +0800

    使用 RxJS 的寫法

---
+++ src/app/tables/table.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { TableItem } from '../table-item';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableService {
+
+  // httpClient = inject(HttpClient);
+
+  constructor(private httpClient:HttpClient) {}
+
+  getData(num: number) {
+    return this.httpClient
+      .get<TableItem[]>(
+        'https://run.mocky.io/v3/53923ca1-98ac-4ce3-a47c-b0af3108de3e'
+      )
+      .pipe(map((result) => result.slice(0, num)));
+  }
+}
--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { TableItem } from '../table-item';
+import { TableService } from './table.service';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-tables',
@@ -14,17 +16,32 @@ import { TableItem } from '../table-item';
 export class TablesComponent {
   route = inject(ActivatedRoute);
   httpClient = inject(HttpClient);
-  data: TableItem[] = [];
+  // data: TableItem[] = [];
+  tableService = inject(TableService);
+  data$ = this.route.queryParamMap
+    .pipe(
+      map((params) => +(params.get('num') || '10')),
+      switchMap((num) => this.tableService.getData(num))
+    );
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.route.queryParamMap.subscribe((params) => {
-        console.log(params.get('num'));
-        this.httpClient.get<TableItem[]>('https://run.mocky.io/v3/53923ca1-98ac-4ce3-a47c-b0af3108de3e').subscribe(result => {
-          this.data = result.slice(0, +(params.get('num') || '10'));
-        });
-      })
-    })
+    // this.route.paramMap.subscribe(params => {
+    //   this.route.queryParamMap.subscribe((params) => {
+    //     console.log(params.get('num'));
+    //     this.httpClient.get<TableItem[]>('https://run.mocky.io/v3/53923ca1-98ac-4ce3-a47c-b0af3108de3e').subscribe(result => {
+    //       this.data = result.slice(0, +(params.get('num') || '10'));
+    //     });
+    //   })
+    // })
+
+    // this.route.queryParamMap.subscribe((params) => {
+    //   console.log(params.get('num'));
+    //   const num = +(params.get('num') || '5');
+    //   this.httpClient.get<TableItem[]>('https://run.mocky.io/v3/53923ca1-98ac-4ce3-a47c-b0af3108de3e').subscribe(result => {
+    //     // this.data = result.slice(0, +(params.get('num') || '10'));
+    //     this.data = result.slice(0, num);
+    //   });
+    // });
   }
 
 }
