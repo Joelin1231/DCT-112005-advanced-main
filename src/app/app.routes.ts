efea59a5cf00fbf39af503b4d13f72174cbb042d
beck <linyuanhao1231@gmail.com>
2023-05-27 09:59:57 +0800

    實戰演練：套用多頁面路由與元件

---
+++ src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { ChartsComponent } from './charts/charts.component';
+import { TablesComponent } from './tables/tables.component';
+
+export const routes: Routes = [
+  {
+    path:'charts',
+    component: ChartsComponent
+  },
+  {
+    path:'tables',
+    component: TablesComponent
+  }
+];
